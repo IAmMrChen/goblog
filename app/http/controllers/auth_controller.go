9aa8c79973aa450cd0a24df0287e05109b529742
陈艺超 <791293535@qq.com>
2021-05-31 11:38:19 +0800

    注册表单验证

--- app/http/controllers/auth_controller.go
+++ app/http/controllers/auth_controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"cyc/goblog/app/models/user"
+	"cyc/goblog/app/requests"
 	"cyc/goblog/pkg/view"
 	"fmt"
 	"net/http"
@@ -16,27 +17,44 @@ func (*AuthController) Register(w http.ResponseWriter, r *http.Request)  {
 	view.RenderSimple(w, view.D{}, "auth.register")
 }
 
+
+type userForm struct {
+	Name            string `valid:"name"`
+	Email           string `valid:"email"`
+	Password        string `valid:"password"`
+	PasswordConfirm string `valid:"password_confirm"`
+}
+
 // DoRegister 处理注册逻辑
 func (*AuthController) DoRegister(w http.ResponseWriter, r *http.Request)  {
-	// 0.接收变量
-	name := r.PostFormValue("name")
-	email := r.PostFormValue("email")
-	password := r.PostFormValue("password")
-
-	// 1. 表单验证
-	// 2. 验证通过-入库，跳转到首页
+	// 1. 初始化数据
 	_user := user.User{
-		Name: name,
-		Email: email,
-		Password: password,
-	}
-	_user.Create()
-	if _user.ID > 0 {
-		fmt.Fprint(w, "插入成功，ID为 " + _user.GetStringID())
-	} else {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprint(w, "创建用户失败，请联系管理员")
+		Name: 			 r.PostFormValue("name"),
+		Email:           r.PostFormValue("email"),
+		Password:        r.PostFormValue("password"),
+		PasswordConfirm: r.PostFormValue("password_confirm"),
 	}
 
-	// 3. 验证不同过 -- 重新显示表单
+
+	// 2. 表单规则
+	errs := requests.ValidateRegistrationForm(_user)
+
+	if len(errs) > 0 {
+		// 3. 表单不通过 —— 重新显示表单
+		view.RenderSimple(w, view.D{
+			"Errors": errs,
+			"User":   _user,
+		}, "auth.register")
+	} else {
+		// 4. 验证成功，创建数据
+		_user.Create()
+
+		if _user.ID > 0 {
+			http.Redirect(w, r, "/", http.StatusFound)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "注册失败，请联系管理员")
+		}
+	}
+
 }
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/gorilla/mux v1.8.0
 	github.com/pelletier/go-toml v1.9.1 // indirect
 	github.com/stretchr/testify v1.7.0
+	github.com/thedevsaddam/govalidator v1.9.10 // indirect
 	golang.org/x/sys v0.0.0-20210521203332-0cec03c779c1 // indirect
 	gorm.io/driver/mysql v1.1.0
 	gorm.io/gorm v1.21.10
